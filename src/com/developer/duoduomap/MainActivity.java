c90f6326792fff62aa096513d549b732140fea77
lihao <lihao_2011@163.com>
2016-08-23 04:08:02 +0800

    HelloActivity搭建完成

--- src/com/developer/duoduomap/MainActivity.java
+++ src/com/developer/duoduomap/MainActivity.java
@@ -1,15 +1,18 @@
 package com.developer.duoduomap;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		Intent intent = new Intent(MainActivity.this, HelloActivity.class);
+		startActivity(intent);
 	}
 }
